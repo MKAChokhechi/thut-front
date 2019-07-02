76f1f6e69bb6b56002fe5fa978c614c433a29193
amir <amirmohhamadsoltani@gmail.com>
2019-07-03 01:18:32 +0430

    add font slider component

--- src/thut.js
+++ src/thut.js
@@ -1,12 +1,15 @@
 import Header from './components/Header';
 import React,{Component} from 'react';
-import 'bootstrap-v4-rtl/dist/css/bootstrap-rtl.min.css';
-
+import 'font-awesome/css/font-awesome.min.css';
+import Slider from "./components/Slider";
 export  default class Thut extends Component
 {
     render() {
         return(
+            <div id="wrapper">
             <Header/>
+            <Slider />
+            </div>
         );
     }
 }
