3b04d8e573d09a310b26bbdb1d4e49432ff0ae1a
amir <amirmohhamadsoltani@gmail.com>
2019-07-02 13:54:31 +0430

    install bootstrap and create directory and create thut , navbar compnent

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 
+import 'thut'
+import Thut from "./thut";
 ReactDOM.render(
-  < />,
+  <Thut />,
   document.getElementById('root')
 );
---
+++ src/thut.js
@@ -0,0 +1,12 @@
+import Navbar from 'components/navbar';
+import React,{Component} from 'react';
+
+
+export  default class Thut extends Component
+{
+    render() {
+        return(
+            <Navbar/>
+        );
+    }
+}
