fe1536c6050cd007eecaa98be00015f2a799030d
amir <amirmohhamadsoltani@gmail.com>
2019-07-02 18:04:01 +0430

    create header component and add fond,css,assets create

---
+++ src/components/Header.js
@@ -0,0 +1,82 @@
+import React,{Component} from 'react';
+import '../style/style.css';
+import '../style/rtl.css';
+//import '../style/settings.css';
+import '../style/main.css';
+import Navbar from "./Navbar";
+
+export  default class Header extends Component
+{
+    render() {
+        return(
+            <header className="de_header_2">
+
+
+                <div className="info">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-md-9">
+                                <ul className="info-list">
+                                    <li><i className="fa fa-phone"></i>
+                                        تماس رایگان: <strong>1800.899.900</strong></li>
+                                    <li><i className="fa fa-envelope-o"></i>
+                                        ایمیل:
+                                        <strong>contact@yoursite.com</strong></li>
+                                    <li><i className="fa fa-clock-o"></i>
+                                        ساعت کاری: <strong>08:00-17:00</strong></li>
+                                </ul>
+                            </div>
+
+                            <div className="col-md-3">
+
+                                <ul className="social-list">
+
+                                    <li><a target="_blank" href="https://www.facebook.com/"><i
+                                        className="fa fa-facebook"></i></a></li>
+
+
+                                    <li><a target="_blank" href="https://twitter.com/"><i className="fa fa-twitter"></i></a>
+                                    </li>
+
+
+                                    <li><a target="_blank" href="https://plus.google.com"><i
+                                        className="fa fa-google-plus"></i></a></li>
+                                    <li><a target="_blank" href="#"><i className="fa fa-dribbble"></i></a></li>
+
+
+                                    <li><a target="_blank" href="#"><i className="fa fa-rss"></i></a></li>
+
+
+
+                                </ul>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-12">
+                            <div id="logo">
+                                <a href="http://demo.mihanwp.com/archi/">
+                                    <img className="logo-static"
+                                         src="http://demo.archiwp.com/wp-content/themes/archi/images/logo.png"
+                                         alt="آرکی"/>
+                                        <img className="logo-scroll"
+                                             src="http://demo.archiwp.com/wp-content/themes/archi/images/logo.png"
+                                             alt="آرکی" />
+                                </a>
+                            </div>
+                            <span id="menu-btn"></span>
+
+                            <Navbar/>
+                        </div>
+
+                    </div>
+                </div>
+            </header>
+        );
+    }
+}
--- src/thut.js
+++ src/thut.js
@@ -1,12 +1,12 @@
-import Navbar from 'components/navbar';
+import Header from './components/Header';
 import React,{Component} from 'react';
-
+import 'bootstrap-v4-rtl/dist/css/bootstrap-rtl.min.css';
 
 export  default class Thut extends Component
 {
     render() {
         return(
-            <Navbar/>
+            <Header/>
         );
     }
 }
