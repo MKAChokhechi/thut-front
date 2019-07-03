c2e0d216c3c5b209712c1d86d2d60d1c491cfcb1
amir <amirmohhamadsoltani@gmail.com>
2019-07-03 20:08:20 +0430

    update carousel

--- src/components/body.js
+++ src/components/body.js
@@ -1,13 +1,15 @@
 import React,{Component} from 'react';
 import  '../style/body.css';
-import Gallery from "./gallery";
+import Gallery from './gallery';
 import Config from "./config";
 
 export default class Body extends Component{
     render() {
         return (
+            <div>
             <Config/>
-            <Gallery/>
+            <Gallery />
+            </div>
         );
     }
 
