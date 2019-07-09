3948089fc43f08e885c852706941b95aa806e129
amir <amirmohhamadsoltani@gmail.com>
2019-07-09 12:59:29 +0430

    update gallery objects size update Magnific update Home

--- src/components/Magnific.js
+++ src/components/Magnific.js
@@ -1,81 +1,81 @@
 import React,{Component} from 'react';
-import  '../style/Magnific.css';
-import Slider from "./Slider";
+
+//import {Carousel} from "react-responsive-carousel";
 import axios from "axios";
 import imageg from "../assets/loding.gif";
+import {Carousel} from "react-responsive-carousel";
 
 export default class Magnific extends Component{
     constructor($props)
     {
         super($props);
-        this.state={post:{}}
+        this.state={post:0}
     }
-     componentWillReceiveProps(nextProps, nextContext)
-      {
-        if(nextProps.post === 0)
-            return
-        axios.get("http://api.thut.ir/posts/"+nextProps.post+"/")
-            .then(response=>{
-                let  data = response.data;
-                console.log(data);
-                this.setState({post:data});
+    componentDidMount() {
+            axios.get("http://api.thut.ir/posts/"+this.props.post)
+                .then(response=>{
+                    this.setState({post:response.data});
 
-            })
-            .catch(error=>{
-                console.log(error);
-            })
-    }
-     isEmpty(obj) {
-        for(var prop in obj) {
-            if(obj.hasOwnProperty(prop))
-                return false;
-        }
-
-        return true;
-    }
-
-    getMagnfic()
-    {
-        document.getElementsByTagName("html")[0].style="margin-right: 15px; overflow: hidden";
-        const  {post}=this.state;
-        return(
-        <div className="magnific"  >
-
-            <div className="container box-magnific text-right" >
-
-                <span className="glyphicon glyphicon-remove close-btn" onClick={()=>{this.props.close();this.setState({post:{}})}} />
-                <img className="mag-load" src={imageg} style={!this.isEmpty(post)?{display:'none'}:{}} />
-
-                <div className="row  rouw" style={this.isEmpty(post)?{display:'none'}:{}} >
-
-                    <div className="col-lg-8 col-md-8">
-                        <Slider images={"http://thut.ir/"+post.image}/>
-
-                    </div>
-                    <div className="col-lg-4 col-md-4">
-                        <h2>{post.title}</h2>
-                        <div className="detail">
-                            <p>در تاریخ امروز</p>
-                            <p>در تاریخ امروز</p>
-                            <p>در  امروز</p>
-                        </div>
-                        {post.content}
-                    </div>
-                </div>
-
-            </div>
-
-        </div>);
-
+                })
+                .catch(error=>{
+                    console.log(error);
+                })
     }
 
     render()
     {
-        if(this.props.post===0)
-            return(<div></div>);
-        return (
-            this.getMagnfic()
-         );
+        const  {post}=this.state;
+        return(
+            <div className="magnific"  >
+
+                <div className="container box-magnific text-right" >
+
+                    <span className="glyphicon glyphicon-remove close-btn" onClick={this.props.close} />
+                    {   post===0?
+                        <img className="mag-load" src={imageg}/>
+                        :
+                        < div className="row  rouw"  >
+
+                        <div className="col-lg-8 col-md-8">
+
+                            <Carousel autoPlay={true}
+                                      showStatus={false}
+                                      showIndicators={false}
+                                      showThumbs={false}
+                                      infiniteLoop={true}
+                                      emulateTouch={true}>
+                                {post.meta.filter(obj => obj.key === "extra-photo"||obj.key === "main-slider-photo").map((obj, index) => (
+                                    <div key={index}>
+                                        <img
+                                            src={'http://thut.ir/' + (obj.link.image)}
+                                            className="mainCarousel"/>
+                                    </div>
+                                ))}
+                                <div >
+                                    <img
+                                        src={'http://thut.ir/' + (post.image)}
+                                        className="mainCarousel"/>
+                                </div>
+
+                            </Carousel>
+
+
+                        </div>
+                        <div className="col-lg-4 col-md-4">
+                        <h2>{post.title}</h2>
+                        <div className="detail">
+                        <p>در تاریخ امروز</p>
+                        <p>در تاریخ امروز</p>
+                        <p>در  امروز</p>
+                        </div>
+                        {post.content}
+                        </div>
+                        </div>
+                    }
+
+                </div>
+
+            </div>);
     }
 
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,11 @@
 import React,{Component} from 'react';
 import Magnific from "../components/Magnific";
 import Gallery from "../components/Gallery";
-import Slider from "../components/Slider";
 import axios from "axios";
-
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
 import Config from "../components/Config";
-
+import  '../style/Magnific.css';
 
 export  default class Home extends Component
 {
@@ -29,6 +29,7 @@ export  default class Home extends Component
 
     openMagnific(id)
     {
+        document.getElementsByTagName("html")[0].style="margin-right: 15px; overflow: hidden";
         this.setState({post:id});
     }
     closeMagnific()
@@ -40,8 +41,28 @@ export  default class Home extends Component
         const {posts,post} = this.state;
         return(
            <div>
-               <Magnific close={this.closeMagnific.bind(this)} post={post} />
-               <Slider />
+               {post === 0?<div />:
+                   <Magnific close={this.closeMagnific.bind(this)} post={post}/>
+               }
+               {posts.length===0?<div/>:
+                   <Carousel autoPlay={true}
+                             showStatus={false}
+                             showIndicators={false}
+                             showThumbs={false}
+                             infiniteLoop={true}
+                             emulateTouch={true}>
+                       {posts.filter(obj => obj.slider === true).map((obj, index) => (
+                           <div key={index}>
+                               <img
+                                   src={'http://thut.ir/' + (obj.meta.find(ob => ob.key === "main-slider-photo").link.image)}
+                                   className="mainCarousel"/>
+                               <div className="description">اسلاید اول</div>
+                           </div>
+                       ))}
+
+                   </Carousel>
+               }
+
                <Config/>
                <Gallery open={this.openMagnific.bind(this)} posts={posts} />
 
